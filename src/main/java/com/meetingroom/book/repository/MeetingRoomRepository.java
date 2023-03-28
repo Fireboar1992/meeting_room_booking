d8821b799fb2aa6c162d75baf2887f6bdcf7f83e
Fireboar1992 <a22468599@gmail.com>
2023-03-29 02:18:29 +0800

    create repository for table meeting room

---
+++ src/main/java/com/meetingroom/book/repository/MeetingRoomRepository.java
@@ -0,0 +1,19 @@
+package com.meetingroom.book.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import com.meetingroom.book.entity.MeetingRoomEntity;
+
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
+
+public interface MeetingRoomRepository extends JpaRepository<MeetingRoomEntity, Long>{
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	public Optional<MeetingRoomEntity> findByName(String roomName);
+	
+}
